b6d8b36cdd2ef6d3d08165a6ceef3161d7f410b5
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-05-04 16:12:07 +0100

    script for patching
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- bootEmbedded/helm-post-install.sh
+++ bootEmbedded/helm-post-install.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-/galasa/kubectl get svc api-external -o=jsonpath='{.spec.ports[?(@.name=="http")].nodePort}'
+set -e
+
+APIPORT=$(kubectl get svc api-external -o=jsonpath='{.spec.ports[?(@.name=="http")].nodePort}')
+ETCDPORT=$(kubectl get svc etcd-external -o=jsonpath='{.spec.ports[?(@.name=="client")].nodePort}')
+COUCHDBPORT=$(kubectl get svc couchdb-external -o=jsonpath='{.spec.ports[?(@.name=="http")].nodePort}')
+
+echo HOSTNAME     is $1
+echo API     port is $APIPORT
+echo ETCD    port is $ETCDPORT
+echo COUCHDB port is $COUCHDBPORT
+
+kubectl get configmap bootstrap-file -o yaml > bootstrap.yaml
+
+sed "s/etcd:http:\/\/etcd:2379/etcd:http:\/\/$1:$ETCDPORT/g" bootstrap.yaml > bootstrap-new.yaml
+
+kubectl replace -f bootstrap-new.yaml
+
+kubectl rollout restart deployment/api
+
+kubectl rollout status deployment/api -w --timeout=3m
